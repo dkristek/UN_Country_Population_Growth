f74b1ad1272db83fe9b7e1aee4a5bd2d73b9dbcc
David Kristek <djkristek@gmail.com>
2022-07-17 14:01:00 -0500

    config

--- config.py
+++
@@ -1,8 +0,0 @@
-#config for sql server
-# parameters for connection
-param_dic = {
-    "host"      : "localhost",
-    "database"  : "world_population",
-    "user"      : "postgres",
-    "password"  : "V3lv3t87"
-}
